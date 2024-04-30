b4ed5c33b1aeef64190bd4aee22945c169142d1c
alicorn231 <168390617+alicorn231@users.noreply.github.com>
2024-05-01 09:57:51 +1200

    V2
    added while loop to secrete.py, and expanded hi.py

--- secret.py
+++ secret.py
@@ -1,1 +1,5 @@
-print("Did you know that erwgfdasd has a crush on asfgdsfgfg")
+user_input = input("would you like to know the secret?\n:")
+if user_input == "yes":
+    print("Did you know that erwgfdasd has a crush on asfgdsfgfg")
+else:
+    print("boringgg")
