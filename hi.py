b3163d843c7870c860d4fa969d929993fdb02897
alicorn231 <168390617+alicorn231@users.noreply.github.com>
2024-05-01 10:00:28 +1200

    V3
    I broke it

--- hi.py
+++ hi.py
@@ -1,2 +1,2 @@
 print("Hello world")
-print("my name is Alice")
+("my name is Alice")
--- secret.py
+++ secret.py
@@ -1,5 +1,5 @@
 user_input = input("would you like to know the secret?\n:")
-if user_input == "yes":
+user_input == "yes":
     print("Did you know that erwgfdasd has a crush on asfgdsfgfg")
 else:
     print("boringgg")
